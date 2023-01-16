742d5753e097c70872decf65c61702da644aa412
Jangyeji <crush03203@naver.com>
2023-01-16 21:23:44 +0900

    20230116

---
+++ WebStudy05_SpringBoard/src/main/java/kr/or/ddit/board/service/BoardServiceImpl.java
@@ -0,0 +1,108 @@
+package kr.or.ddit.board.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import kr.or.ddit.board.dao.AttatchDAO;
+import kr.or.ddit.board.dao.BoardDAO;
+import kr.or.ddit.board.exception.NotExistBoardException;
+import kr.or.ddit.board.vo.AttatchVO;
+import kr.or.ddit.board.vo.BoardVO;
+import kr.or.ddit.vo.PagingVO;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class BoardServiceImpl implements BoardService {
+	@Inject
+	private BoardDAO boardDAO;
+
+	@Inject
+	private AttatchDAO AttatchDAO;
+	@Inject
+	private PasswordEncoder encoder;
+
+	@Value("#{appInfo.saveFiles}")
+	private File saveFiles;
+
+	@PostConstruct
+	public void init() throws IOException {
+		log.info("EL로 주입된 첨부파일 저장경로 : {}", saveFiles.getCanonicalPath());
+	}
+
+	private int processAttathList(BoardVO board) {
+		List<AttatchVO> attatchList = board.getAttatchList();
+		if (attatchList == null || attatchList.isEmpty())
+			return 0;
+		// 1. metadata 저장 - DB(ATTATCH)
+		int rowcnt = AttatchDAO.insertAttatches(board);
+		// 2. binary 저장 - Middle Tier (D:\saveFiles)
+		try {
+			for (AttatchVO attatch : attatchList) {
+				if(1==1)
+					throw new RuntimeException("강제 발생 예외");
+				attatch.saveTo(saveFiles);
+			}
+			return rowcnt;
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+	@Transactional
+	@Override
+	public int createBoard(BoardVO board) {
+		String plain = board.getBoPass();
+		String encoded = encoder.encode(plain);
+		board.setBoPass(encoded);
+
+		int rowcnt = boardDAO.insertBoard(board);
+		// 첨부 파일 등록
+		rowcnt += processAttathList(board);
+
+		return rowcnt;
+	}
+
+	@Override
+	public void retrieveBoardList(PagingVO<BoardVO> pagingVO) {
+		pagingVO.setTotalRecord(boardDAO.selectTotalRecord(pagingVO));
+		pagingVO.setDataList(boardDAO.selectBoardList(pagingVO));
+	}
+
+	@Override
+	public BoardVO retrieveBoard(int boNo) {
+		BoardVO board = boardDAO.selectBoard(boNo);
+		if (board == null)
+			throw new NotExistBoardException(boNo);
+		boardDAO.incrementHit(boNo);
+		return board;
+	}
+
+	@Override
+	public int modifyBoard(BoardVO board) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int removeBoard(int boNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public AttatchVO retrieveForDownload(int attNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ WebStudy05_SpringBoard/webapp/WEB-INF/jsp/board/boardView.jsp
@@ -0,0 +1,47 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>	
+<%@ taglib uri="http://www.springframework.org/tags" prefix="spring" %>	
+<table class="table table-bordered">
+	<tr>
+		<th><spring:message code="board.boTitle" /></th>
+		<td>${board.boTitle}</td>
+	</tr>
+	<tr>
+		<th><spring:message code="board.boWriter" /></th>
+		<td>${board.boWriter}[${board.boMail}]</td>
+	</tr>
+	<c:if test="${not empty board.attatchList }">
+		<tr>
+			<th>첨부파일</th>
+			<td>
+				<c:forEach items="${board.attatchList }" var="attatch" varStatus="vs">
+					${attatch.attFilename } ${vs.last?"":"," }
+				</c:forEach>
+			</td>
+		</tr>
+	</c:if>
+	<tr>
+		<th><spring:message code="board.boDate" /></th>
+		<td>${board.boDate}</td>
+	</tr>
+	<tr>
+		<th><spring:message code="board.boIp" /></th>
+		<td>${board.boIp}</td>
+	</tr>
+	<tr>
+		<th><spring:message code="board.boHit" /></th>
+		<td>${board.boHit}</td>
+	</tr>
+	<tr>
+		<th><spring:message code="board.boContent" /></th>
+		<td>${board.boContent}</td>
+	</tr>
+	<tr>
+		<td colspan="2">	
+			<a class="btn btn-primary" href="">수정</a>
+			<a class="btn btn-danger" href="">삭제</a>
+			<a class="btn btn-secondary" href="<c:url value='/board/boardList.do'/>">목록으로</a>
+		</td>
+	</tr>
+</table>
